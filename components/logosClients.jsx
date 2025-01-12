34f08b221250fc8ed137294e67d7bdcc42515125
jchris3434 <jc.fontaine34@gmail.com>
2025-01-12 21:34:17 +0100

    front jc ok v1

---
+++ components/logosClients.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "../styles/logosClients.css";
+
+const logos = [
+  "Airbnb.png",
+  "Amazon.png",
+  "Atlassian.png",
+  "BookMyShow.png",
+  "FedEx.png",
+  "Gatsby.png",
+  "Google.png",
+  "Grubhub.png",
+  "Hubspot.png",
+  "Microsoft.png",
+  "OLA.png",
+  "OYO.png",
+  "Servicenow.png",
+  "Strapi.png",
+  "Walmart.png"
+];
+
+const LogosClients = () => {
+  return (
+    <section className="logos-section">
+      <div className="logos-container">
+        {logos.map((logo, index) => (
+          <div key={index} className="logo-item">
+            <img src={`/logos/${logo}`} alt={logo.split('.')[0]} className="logo-img" />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('logosClients-root'));
+root.render(<LogosClients />);
---
+++ components/testimonials.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import "../styles/testimonials.css";
+
+const Testimonials = () => {
+  const [testimonials, setTestimonials] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  console.log('test recup api', testimonials);
+
+  // Appel de l'API pour récupérer les testimonials du back
+  useEffect(() => {
+    const fetchTestimonials = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/api/testimonials/top');
+        const data = await response.json();
+        setTestimonials(data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Erreur de récupération du json testimonials', error);
+        setLoading(false);
+      }
+    };
+
+    fetchTestimonials();
+  }, []);
+
+  if (loading) {
+    return <h2>Chargement...</h2>;
+  }
+
+  return (
+    <section className='testimonials-wrapper'>
+      <h1 className='testi-title'>Our clients speak</h1>
+      <div className="testimonials-card-container">
+        {testimonials.map((testimonial, index) => (
+          <div key={index} className="testimonial-card">
+            <div className="testimonial-text-wrapper">
+              <h3 className='testimonial-client-title'>{testimonial.title}</h3>
+              <span className='testimonial-client-comment'>{testimonial.body}</span>
+            </div>
+            <div className="testimonial-client-wrapper">
+              <img className="testimonial-client-avatar" src={testimonial.personPicture} alt={`Photo of ${testimonial.personName}`} />
+              <span className="testimonial-client-name">{testimonial.personName}</span>
+              <span className="testimonial-client-company">{testimonial.personCompany}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('testimonials-root'));
+root.render(<Testimonials />);
