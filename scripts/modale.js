b57f33f5c49f0302bb4611cf68380d2025df6aed
jchris3434 <jc.fontaine34@gmail.com>
2025-01-15 12:35:58 +0100

    add keyup escape modale

--- scripts/modale.js
+++ scripts/modale.js
@@ -1,26 +1,36 @@
 // acces a la modale
 let modal = document.getElementById("modal");
-    
-// Fonction pour afficher la modale
+
 function showModal() {
   modal.style.display = "block";
 }
 
-// ferme la modal si click hors champs
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// ferme la modale si clic hors champs
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    closeModal();
   }
-}
+};
+
+// Ferme la modale avec la touche echap
+window.addEventListener("keyup", function(event) {
+  if (event.key === "Escape") { 
+    closeModal();
+  }
+});
 
 // détection position du curseur souris
 window.addEventListener("mousemove", function(event) {
   // verification si le curseur est dans le coin supérieur droit
   let x = event.clientX; // Position X du curseur
   let y = event.clientY; // Position Y du curseur
-  let screenWidth = window.innerWidth; // largeur fenêtre
+  let screenWidth = window.innerWidth; // largeur de la fenêtre
 
-  // Affiche la modale si le curseur est dans le coin supérieur droit (10px de marge)
+  // Affiche la modale si le curseur est dans le coin superieur droit
   if (x > screenWidth - 100 && y < 100) {
     showModal();
   }
